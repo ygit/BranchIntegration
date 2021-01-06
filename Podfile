5b8696694b23c123a3bc4ee27c4199a059d9c515
Yogesh Singh <yogesh.singh.2810@gmail.com>
2021-01-06 17:00:27 +0530

    added CleverTap

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'BranchIntegration (iOS)' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for BranchIntegration (iOS)
+  pod 'CleverTap-iOS-SDK' 
+
+end
+
+target 'BranchIntegration (macOS)' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for BranchIntegration (macOS)
+
+end
--- Shared/BranchIntegrationApp.swift
+++ Shared/BranchIntegrationApp.swift
@@ -6,9 +6,21 @@
 //
 
 import SwiftUI
+import UIKit
+import CleverTapSDK
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        CleverTap.autoIntegrate()
+        return true
+    }
+}
 
 @main
 struct BranchIntegrationApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
