bac747b6f3d4166ce0a6f8fae9a93ae963bb4d20
Yogesh Singh <yogesh.singh.2810@gmail.com>
2021-01-06 16:39:26 +0530

    Initial Commit

---
+++ Shared/BranchIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BranchIntegrationApp.swift
+//  Shared
+//
+//  Created by Yogesh Singh on 06/01/21.
+//
+
+import SwiftUI
+
+@main
+struct BranchIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
